3c8fcc4d83c80d4ae9c2b24cb22fd5ce02e88f89
GabbyHE <33002687+GabbyHE@users.noreply.github.com>
2017-10-22 22:27:17 +0800

    Add files via upload

---
+++ compile.m
@@ -0,0 +1,26 @@
+mex -setup
+cd ./Dependencies
+cd ./generateSuperpixels
+mex SLIC.cpp
+cd ..
+
+cd ./createIndexTree/multi-segmentation
+mex -v mexMergeAdjRegs_Felzenszwalb.cpp
+cd ..
+cd ..
+
+cd ./extractFeatures/matlabPyrTools/MEX
+compilePyrTools
+cd ..
+cd ..
+
+cd ./edison_matlab_interface
+compile_edison_wrapper
+cd ..
+cd ..
+
+cd ./postprocessing/propagation
+mex vgg_kmiter.cxx
+cd ..
+cd ..
+
---
+++ readme.txt
@@ -0,0 +1,7 @@
+usages:
+1.run compile.m
+2.run demo.m
+
+
+Some codes  are from paper "Houwen Peng, Bing Li, Haibin Ling, Weiming Hu, Weihua Xiong, Stephen J. Maybank:
+Salient Object Detection via Structured Matrix Decomposition. IEEE Trans. Pattern Anal. Mach. Intell. 39(4): 818-832 (2017).
